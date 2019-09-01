424fb4e6a244da20679c91c07a1d84e4efe08fbf
ronnie <wondra@163.com>
2019-09-01 16:56:34 +0800

    update base router

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ const routerBase =
   process.env.DEPLOY_ENV === 'GH_PAGES'
     ? {
         router: {
-          base: '/zhangsheng2/'
+          base: '/sang-cheung-nuxt/'
         }
       }
     : {};
